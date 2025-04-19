d1cada56113a1d3bc64555be1ef5a22d17d5b030
Muhammadjoni <mrahimzod@gmail.com>
2025-04-19 21:46:21 +0500

    added review models && schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_19_101421) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_19_120036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -33,6 +33,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_19_101421) do
     t.string "name"
     t.decimal "price"
     t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.float "rating"
+    t.text "commment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
