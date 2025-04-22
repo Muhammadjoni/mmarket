e699d7fa02f2687101505d5c28763a5ff3a28a58
Muhammadjoni <mrahimzod@gmail.com>
2025-04-22 09:15:00 +0500

    product logic & and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,49 @@
 class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+    end
+
+    def show 
+        @product = Product.find(params[:id])
+        # @reviews = @product.reviews
+        # @review = Review.new
+    end
+
+    def new 
+        @product = Product.new
+    end 
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to products_path(@product), notice: "Product was successfully created."
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to products_path(@product), notice: "Product was successfully updated."    
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+
+        redirect_to products_path, status: :see_other
+    end
+    
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :price, :stock, :in_stock)   
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "store", to: "pages#store", as: :store
   get "about", to: "pages#about", as: :about
+
+  get "products", to: "products#index", as: :products
+  get "products/:id", to: "products#show", as: :product
+  get "products/new", to: "products#new",   as: :new_product
+  post "products", to: "products#create", as: :create_product
+  get "products/:id/edit", to: "products#edit", as: :edit_product
+  patch "products/:id", to: "products#update",  as: :update_product
+  delete "products/:id", to: "products#destroy", as: :destroy_product
 end
