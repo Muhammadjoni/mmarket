e3b22d0c3094d84f5301aedb08a3d8fd14157929
Muhammadjoni <mrahimzod@gmail.com>
2025-04-26 03:44:32 +0500

    devise and other gems

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 gem 'cloudinary'
-gem "dotenv-rails"
+# gem "dotenv-rails"
 
 
 gem "bootstrap", "~> 5.2"
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,11 @@
 class Product < ApplicationRecord
     belongs_to :user
 
-    has_one_attached :image
     has_many :reviews, dependent: :destroy
     has_many :order_products, dependent: :destroy
     has_many :orders, through: :order_products
     validates :name, presence: true
     validates :price, presence: true
-    has_many_attached :images
+    has_many_attached :photos
     
 end
